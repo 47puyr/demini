fa34eb8143f94d1b41be049084b7a7141576a0c1
47puyr <143574840+47puyr@users.noreply.github.com>
2024-03-22 09:48:03 +0700

    Bai 1.cpp
    Sử dụng hàm trong ngôn ngữ C, viết chương trình xuất các số nguyên có 2 chữ số và là bội của 7.

--- 2001216021_NguyenHoangPhat_Buoi2/Bai 1.cpp
+++ 2001216021_NguyenHoangPhat_Buoi2/Bai 1.cpp
@@ -1,12 +1,12 @@
 //bai 1
 #include <stdio.h>
 
-// Hàm ki?m tra xem m?t s? có ph?i là b?i c?a 7 hay không
+// HÃ m ki?m tra xem m?t s? cÃ³ ph?i lÃ  b?i c?a 7 hay khÃ´ng
 int isMultipleOf7(int num) {
     return (num % 7 == 0);
 }
 
-// Hàm xu?t các s? nguyên có 2 ch? s? và là b?i c?a 7
+// HÃ m xu?t cÃ¡c s? nguyÃªn cÃ³ 2 ch? s? vÃ  lÃ  b?i c?a 7
 void printMultiplesOf7() {
     printf("Cac so nguyen co 2 chu so va la boi cua 7:\n");
     for (int i = 10; i < 100; i++) {
@@ -18,7 +18,7 @@ void printMultiplesOf7() {
 }
 
 int main() {
-    printMultiplesOf7(); // G?i hàm d? xu?t các s? nguyên có 2 ch? s? và là b?i c?a 7
+    printMultiplesOf7(); // G?i hÃ m d? xu?t cÃ¡c s? nguyÃªn cÃ³ 2 ch? s? vÃ  lÃ  b?i c?a 7
     return 0;
 }
 
